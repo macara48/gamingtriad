20f7534b5c5cb5b59105d65993e7be8c4b6edcfb
macara48 <80272737+macara48@users.noreply.github.com>
2022-09-01 20:22:44 -0300

    Add files via upload

---
+++ gamingtriad/gamingtriad/src/main/java/projetojogo/gamingtriad/requests/JogoPutRequestBody.java
@@ -0,0 +1,9 @@
+package projetojogo.gamingtriad.requests;
+
+import lombok.Data;
+
+@Data
+public class JogoPutRequestBody {
+    private Long id;
+    private String name;
+}
---
+++ gamingtriad/gamingtriad/src/test/java/projetojogo/gamingtriad/GamingtriadApplicationTests.java
@@ -0,0 +1,13 @@
+package projetojogo.gamingtriad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingtriadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
